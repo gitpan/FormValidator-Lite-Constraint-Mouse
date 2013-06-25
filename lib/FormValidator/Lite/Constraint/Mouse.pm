06fa58a3f2b062372ff19e9e912cf8f3aa51b78a
Hideaki Ohno <hideakio@cpan.org>
2013-06-25 23:11:38 +0000

    Import of HIDEAKIO/FormValidator-Lite-Constraint-Mouse-0.04 from CPAN.
    gitpan-cpan-distribution: FormValidator-Lite-Constraint-Mouse
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         HIDEAKIO/FormValidator-Lite-Constraint-Mouse-0.04.tar.gz
    gitpan-cpan-author:       HIDEAKIO
    gitpan-cpan-maturity:     released

--- lib/FormValidator/Lite/Constraint/Mouse.pm
+++ lib/FormValidator/Lite/Constraint/Mouse.pm
@@ -4,7 +4,7 @@ use warnings;
 use FormValidator::Lite::Constraint;
 use Mouse::Util::TypeConstraints ();
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 my $get_constraint = \&Mouse::Util::TypeConstraints::find_type_constraint;
 
@@ -59,7 +59,7 @@ FormValidator::Lite::Constraint::Mouse - Use Mouse's type constraints.
 =head1 DESCRIPTION
 
 This module provides Mouse's type constraint as constraint rule of L<FormValidator::Lite>
-If you wanna know the constraint, see L<Mouse::Util::TypeConstraints> for details.
+If you want to know the constraint, see L<Mouse::Util::TypeConstraints> for details.
 
 =head1 AUTHOR
 
